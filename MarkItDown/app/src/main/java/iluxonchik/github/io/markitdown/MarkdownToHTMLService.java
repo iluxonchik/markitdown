499e8c8814d6d2a50469203f54cbf4edbcf31f1b
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-08-05 17:18:03 +0100

    MarkdownToHTMLService.java addtions.
    * Not yet tested.
    * Not yet refractored.

--- MarkItDown/app/src/main/java/iluxonchik/github/io/markitdown/MarkdownToHTMLService.java
+++ MarkItDown/app/src/main/java/iluxonchik/github/io/markitdown/MarkdownToHTMLService.java
@@ -1,10 +1,14 @@
 package iluxonchik.github.io.markitdown;
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.webkit.WebView;
 
 public class MarkdownToHTMLService extends IntentService {
 
@@ -13,11 +17,16 @@ public class MarkdownToHTMLService extends IntentService {
     private final int NULL_NOTE = -1;
     private final int EDITED_POS = 0;
     private final int FALSE = 0;
+    private final int MD_POS = 0;
+    private final String LIMIT_1 = "1";
+
+    private final String MD_TO_HTML_SERVICE_LOGTAG = "MarkdownToHTMLService";
 
 
     private int noteId;
     private MarkItDownDbHelper dbHelper;
     private SQLiteDatabase readableDb;
+    private Cursor cursor;
 
     public MarkdownToHTMLService() {
         super("MarkdownToHTMLService");
@@ -25,37 +34,84 @@ public class MarkdownToHTMLService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d(MD_TO_HTML_SERVICE_LOGTAG, "Starting service...");
+
         if (intent == null) return;
-        noteId = intent.getIntExtra(EXTRA_NOTE_ID, -1);
+        noteId = intent.getIntExtra(EXTRA_NOTE_ID, NULL_NOTE);
         if (noteId == NULL_NOTE) return;
+
         dbHelper = new MarkItDownDbHelper(this);
         readableDb = dbHelper.getReadableDatabase();
 
-        Cursor cursor = readableDb.query(MarkItDownDbContract.Notes.TABLE_NAME,
-                new String[]{ MarkItDownDbContract.Notes.COLUMN_NAME_EDITED}, "_id = ?",
-                new String[]{Integer.toString(noteId)}, null, null, null, null);
+        cursor = readableDb.query(MarkItDownDbContract.Notes.TABLE_NAME,
+                new String[]{MarkItDownDbContract.Notes.COLUMN_NAME_EDITED}, "_id = ?",
+                new String[]{Integer.toString(noteId)}, null, null, null, LIMIT_1);
 
         if (cursor.moveToFirst() == false) {
-            cleanup();
+            Log.d(MD_TO_HTML_SERVICE_LOGTAG, "Cursor empty.");
             return;
         }
 
         int isNoteEdited = cursor.getInt(EDITED_POS);
-
         if (isNoteEdited == FALSE) {
             // TODO: broadcast that note is updated
+            Log.d(MD_TO_HTML_SERVICE_LOGTAG, "Note has an updated HTML");
+            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(ACTION_COMPLETE));
+
         }
 
         // TODO: convert md to html, store html in db, set edited column val to 0, broadcast that
         // note is updated.
 
+        // Get markdown text
+        cursor = readableDb.query(MarkItDownDbContract.Notes.TABLE_NAME,
+                new String[]{MarkItDownDbContract.Notes.COLUMN_NAME_TEXT_MARKDOWN}, "_id = ?",
+                new String[]{Integer.toString(noteId)}, null, null, null, LIMIT_1);
 
+        if (cursor.moveToFirst() == false) {
+            Log.d(MD_TO_HTML_SERVICE_LOGTAG, "Cursor empty.");
+            return;
+        }
 
+        // Convert markdown to HTML
+        MarkdownProcessor txtmark = new TxtmarkProcessor();
+        String html = txtmark.mdToHTML(cursor.getString(MD_POS));
+        Log.d(MD_TO_HTML_SERVICE_LOGTAG, "HTML result: \n" + html);
 
+        // Put HTML in DB
+        SQLiteDatabase writableDb = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MarkItDownDbContract.Notes.COLUMN_NAME_TEXT_HTML, html);
+        contentValues.put(MarkItDownDbContract.Notes.COLUMN_NAME_EDITED, FALSE);
+
+        writableDb.update(MarkItDownDbContract.Notes.TABLE_NAME, contentValues, "_id = ?",
+                new String[]{Integer.toString(noteId)});
+
+        writableDb.close();
+
+        // Broadcast
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(ACTION_COMPLETE));
 
     }
 
-    private void cleanup() {
-        // TODO
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (cursor != null) {
+            Log.d(MD_TO_HTML_SERVICE_LOGTAG, "Closing cursor");
+            cursor.close();
+        }
+
+        if (readableDb != null) {
+            Log.d(MD_TO_HTML_SERVICE_LOGTAG, "Closing readableDb");
+            readableDb.close();
+        }
+
+        if (dbHelper != null) {
+            Log.d(MD_TO_HTML_SERVICE_LOGTAG, "Closing dbHelper");
+            dbHelper.close();
+        }
     }
+
 }
